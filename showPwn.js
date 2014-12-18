20c18388759843a0ae2a1c3863623f9a30e0272a
tituman <tituman@users.noreply.github.com>
2014-12-18 09:44:29 +0100

    merged with frames branch

--- showPwn.js
+++ showPwn.js
@@ -1,4 +1,4 @@
-javascript:var b64pad='';var chrsz=8;var inputArray=new Array();var specialIterations=[{domains:['fh-wels.at','fh-ooe.at','fhooe.at'],iter:4,addOnes:true},{domains:['cardcomplete.com'],iter:1,addOnes:true},{domains:['google.com','google'],iter:1,addOnes:false},{domains:['evernote.com'],iter:1,addOnes:false}];function b64_sha1(s){return binb2b64(core_sha1(str2binb(s),s.length*chrsz));}
+javascript:var b64pad='';var DEBUG=true;var chrsz=8;var inputArray=new Array();var specialIterations=[{domains:['fh-wels.at','fh-ooe.at','fhooe.at'],iter:4,addOnes:true},{domains:['cardcomplete.com'],iter:1,addOnes:true},{domains:['google.com','google'],iter:1,addOnes:false},{domains:['evernote.com'],iter:1,addOnes:false}];function b64_sha1(s){return binb2b64(core_sha1(str2binb(s),s.length*chrsz));}
 function core_sha1(x,len){x[len>>5]|=0x80<<(24-len);x[((len+64>>9)<<4)+15]=len;var w=Array(80);var a=1732584193;var b=-271733879;var c=-1732584194;var d=271733878;var e=-1009589776;for(var i=0;i<x.length;i+=16){var olda=a;var oldb=b;var oldc=c;var oldd=d;var olde=e;for(var j=0;j<80;j++){if(j<16)w[j]=x[i+j];else w[j]=rol(w[j-3]^w[j-8]^w[j-14]^w[j-16],1);var t=safe_add(safe_add(rol(a,5),sha1_ft(j,b,c,d)),safe_add(safe_add(e,w[j]),sha1_kt(j)));e=d;d=c;c=rol(b,30);b=a;a=t;}
 a=safe_add(a,olda);b=safe_add(b,oldb);c=safe_add(c,oldc);d=safe_add(d,oldd);e=safe_add(e,olde);}
 return Array(a,b,c,d,e);}
@@ -13,13 +13,15 @@ function getInputs(document){var myInputs;if(myInputs=document.getElementsByTagN
 function traverseFrames(document){try{getInputs(document);}catch(e){console.log('ERROR: ',e);};if(document){var fs=document.getElementsByTagName('frame');for(var i=0;i<fs.length;++i){try{traverseFrames(fs[i].contentDocument);}catch(e){console.log('ERROR: ',e);};}
 fs=document.getElementsByTagName('iframe');for(var i=0;i<fs.length;++i){try{traverseFrames(fs[i].contentDocument);}catch(e){console.log('ERROR: ',e);};}}}
 function doIt(){var domain='';var flagShow=false;var flagWritePWN=false;var flagTypeInDomain=false;var host1=window.location.hostname;if(host1!=null){if(sld=host1.match(/([^.]+\.([a-z][a-z][a-z]|[a-z][a-z]\.[a-z][a-z]|[a-z][a-z]))$/i)){domain=sld[0];}else{try{domain=host1.match(/([^.]+\.[^.]+\.[a-z][a-z])$/i)[0];window.alert('second match: '+domain);}catch(err){domain=window.prompt('host not recognized, type in custom domain');flagTypeInDomain=true;}}}else{domain=window.prompt('host not recognized, type in custom domain');flagTypeInDomain=true;}
-if(!flagTypeInDomain){var Domain=window.prompt('Enter your domain');flagShow=true;if(Domain!=null&&Domain!=''){domain=Domain;flagTypeInDomain=true;}}
+if(DEBUG){if(!flagTypeInDomain){var Domain=window.prompt('Enter your domain');flagShow=true;if(Domain!=null&&Domain!=''){domain=Domain;flagTypeInDomain=true;}}}
 for(i=0;i<specialIterations.length;i++){for(j=0;j<specialIterations[i].domains.length;j++){if((domain.indexOf(specialIterations[i].domains[j])!=-1)){domain=specialIterations[i].domains[0]+specialIterations[i].iter;var addOnes=specialIterations[i].addOnes;}}}
 var p;var i=0,j=0;traverseFrames(document);var wrotePWAtLeastOnce=false;var wrotePWAtLeastOnceSecondChance=false;for(i=0;i<inputArray.length;i++){flagWritePWN=false;if(inputArray[i]==null||inputArray[i]==''||inputArray[i]==0){continue;}
 D=inputArray[i];if(D.type=='password'){if(D.value||D.value!=''){p=b64_sha1(D.value+':'+domain).substr(0,8)+'1a';if(addOnes){p=p+'11';}
-flagWritePWN=window.prompt(domain,p);if(flagShow){if(flagWritePWN){D.value=p;D.focus();D.style.background='#FF69B4';wrotePWAtLeastOnce=true;}}else{D.value=p;D.focus();D.style.background='#FF69B4';wrotePWAtLeastOnce=true;}}}
+if(DEBUG){flagWritePWN=window.prompt(domain,p);}
+if(flagShow){if(flagWritePWN){D.value=p;D.focus();D.style.background='#FF69B4';wrotePWAtLeastOnce=true;}}else{D.value=p;D.focus();D.style.background='#FF69B4';wrotePWAtLeastOnce=true;}}}
 if(D.type=='text'){if(D.value||D.value!=''){if(D.name.toUpperCase().indexOf('PASSWORD')!=-1||D.name.toUpperCase().indexOf('PASSWD')!=-1){p=b64_sha1(D.value+':'+domain).substr(0,8)+'1a';if(addOnes){p=p+'11';}
-flagWritePWN=window.prompt(domain,p);if(flagShow){if(flagWritePWN){D.value=p;D.focus();D.style.background='#FF69B4';wrotePWAtLeastOnce=true;}}else{D.value=p;D.focus();D.style.background='#FF69B4';wrotePWAtLeastOnce=true;}}}}}
+if(DEBUG){flagWritePWN=window.prompt(domain,p);}
+if(flagShow){if(flagWritePWN){D.value=p;D.focus();D.style.background='#FF69B4';wrotePWAtLeastOnce=true;}}else{D.value=p;D.focus();D.style.background='#FF69B4';wrotePWAtLeastOnce=true;}}}}}
 if(wrotePWAtLeastOnce==false){var pwClear=window.prompt('enter pw in clear');if(pwClear){p=b64_sha1(pwClear+':'+domain).substr(0,8)+'1a';if(addOnes){p=p+'11';}
 window.prompt(domain,p);}}}
 doIt();void(null);
