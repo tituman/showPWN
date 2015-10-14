c4fa3f26bf2fa27a8f30aa60f309b3d082389531
tituman <tituman@users.noreply.github.com>
2015-10-14 17:49:01 -0500

    Update showPwn_pretty.js

--- showPwn_pretty.js
+++ showPwn_pretty.js
@@ -40,7 +40,7 @@ Domain iterations:
 */
 
 var specialIterations = [
-{domains : ['fh-wels.at','fh-ooe.at','fhooe.at'], 	iter : 5, addOnes : true},
+{domains : ['fh-wels.at','fh-ooe.at','fhooe.at'], 	iter : 6, addOnes : true},
 {domains : ['cardcomplete.com'], 					iter : 1, addOnes : true},
 {domains : ['google.com', 'google'], 				iter : 2, addOnes : false},
 {domains : ['ebay'], 								iter : 1, addOnes : false},
