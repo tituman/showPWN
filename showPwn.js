f700d30a04e739033b40c4617464bd735fa936c1
tituman <krobeczu@gmail.com>
2015-11-04 15:05:53 +0100

    14

--- showPwn.js
+++ showPwn.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 javascript:var b64pad='';var DEBUG=false;var chrsz=8;var inputArray=new Array();var specialIterations=[{domains:['fh-wels.at','fh-ooe.at','fhooe.at'],iter:6,addOnes:true},{domains:['cardcomplete.com'],iter:1,addOnes:true},{domains:['google.com','google'],iter:2,addOnes:false},{domains:['ebay'],iter:1,addOnes:false},{domains:['evernote.com'],iter:1,addOnes:false}];function b64_sha1(s){return binb2b64(core_sha1(str2binb(s),s.length*chrsz));}
-=======
-javascript:var b64pad='';var DEBUG=true;var chrsz=8;var inputArray=new Array();var specialIterations=[{domains:['fh-wels.at','fh-ooe.at','fhooe.at'],iter:6,addOnes:true},{domains:['cardcomplete.com'],iter:1,addOnes:true},{domains:['google.com','google'],iter:2,addOnes:false},{domains:['ebay'],iter:1,addOnes:false},{domains:['evernote.com'],iter:1,addOnes:false}];function b64_sha1(s){return binb2b64(core_sha1(str2binb(s),s.length*chrsz));}
->>>>>>> refs/remotes/origin/gh-pages
 function core_sha1(x,len){x[len>>5]|=0x80<<(24-len);x[((len+64>>9)<<4)+15]=len;var w=Array(80);var a=1732584193;var b=-271733879;var c=-1732584194;var d=271733878;var e=-1009589776;for(var i=0;i<x.length;i+=16){var olda=a;var oldb=b;var oldc=c;var oldd=d;var olde=e;for(var j=0;j<80;j++){if(j<16)w[j]=x[i+j];else w[j]=rol(w[j-3]^w[j-8]^w[j-14]^w[j-16],1);var t=safe_add(safe_add(rol(a,5),sha1_ft(j,b,c,d)),safe_add(safe_add(e,w[j]),sha1_kt(j)));e=d;d=c;c=rol(b,30);b=a;a=t;}
 a=safe_add(a,olda);b=safe_add(b,oldb);c=safe_add(c,oldc);d=safe_add(d,oldd);e=safe_add(e,olde);}
 return Array(a,b,c,d,e);}
