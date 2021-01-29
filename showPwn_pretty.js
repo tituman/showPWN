4e6217b7f1d944c8a174773a460bf1de132cc91b
pnd Peon de la Parra Daniel <pnd@keba.com>
2021-01-29 11:26:51 +0100

    t5

--- showPwn_pretty.js
+++ showPwn_pretty.js
@@ -183,8 +183,8 @@ function traverseFrames(document){
 
 function doIt() {
 	
-import specialIterationsExt from 'printS.js';
-//window.alert('external data: \n' + specialIterationsExt);
+//import specialIterationsExt from 'printS.js';
+window.alert('external data: \n' + specialIterationsExt);
 
 /* get info about which domain should the password be calculated with */
     var domain = '';
