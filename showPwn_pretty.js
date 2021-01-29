b6a84fdc54ce263ce9915c61cee9673389f9e3a6
pnd Peon de la Parra Daniel <pnd@keba.com>
2021-01-29 16:06:26 +0100

    should work

--- showPwn_pretty.js
+++ showPwn_pretty.js
@@ -5,6 +5,11 @@ var chrsz = 8;
 
 var inputArray = new Array();
 /*\
+*
+* v25
+* added thalia, amazon, 
+* made external data file
+*
 * v24
 * added slack
 *
@@ -67,25 +72,6 @@ Domain iterations:
 
 */
 var specialIterations = new Array();
-
-var specialIterations1 = [
-{domains : ['fh-wels.at','fh-ooe.at','fhooe.at'], 	iter : 9, addOnes : true},
-{domains : ['cardcomplete.com'], 					iter : 1, addOnes : true},
-{domains : ['google.com', 'google'], 				iter : 2, addOnes : false},
-{domains : ['ebay'], 								iter : 1, addOnes : false},
-{domains : ['evernote.com'], 						iter : 1, addOnes : false},
-{domains : ['pearl.de', 'pearl.at'], 				iter : 0, addOnes : false},
-{domains : ['miles-and-more.com', 'worldshop.eu'],	iter : 0, addOnes : false},
-{domains : ['shroomery'],							iter : 0, addOnes : false},
-{domains : ['pinterest'],							iter : 0, addOnes : false},
-{domains : ['live'],								iter : 4, addOnes : false}, /*for stupid personal email*/
-{domains : ['quora'],								iter : 0, addOnes : false},
-{domains : ['slack'],								iter : 0, addOnes : false},
-{domains : ['thalia'],								iter : 0, addOnes : false},
-{domains : ['amazon'],								iter : 0, addOnes : false},
-{domains : ['ts.today'],							iter : 0, addOnes : false}
-];
-
 
 function b64_sha1(s) {
     return binb2b64(core_sha1(str2binb(s), s.length * chrsz));
